551f8af51ba442785eaac2435d923aacae973df6
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 23:25:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,6 +13,7 @@ def directors_totals(nds)
       while movie_index < nds[director_index][:movies].length do
           result[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
             movie_index += 1
+end
 
 
 
